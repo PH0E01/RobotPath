4b750fcd9d3bd824b3417af933b727f1643348f6
PH0E01 <phoetears01@gmail.com>
2024-07-13 11:56:18 +0300

    changing the synchronization

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,32 @@ import java.util.Random;
 
 public class Main {
 
-        public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
+    public static final Map<Integer, Integer> sizeToFreq = new HashMap<>();
 
-        public static void main(String[] args){
-            int numThreads = 1000;
-            Thread[] threads = new Thread[numThreads];
+    public static void main(String[] args) {
+        int numThreads = 1000;
+        Thread[] threads = new Thread[numThreads];
 
-            for(int i = 0; i < numThreads; i++) {
-                threads[i] = new Thread(() -> {
-                    String route = generateRoute("RLRFR", 100);
-                    int count = countTurns(route);
-                    updateSizeToFreq(count);
-                    System.out.println("Thread: " + Thread.currentThread().getId() + ", Count: " + count);
+        for (int i = 0; i < numThreads; i++) {
+            threads[i] = new Thread(() -> {
+                String route = generateRoute("RLRFR", 100);
+                int count = countTurns(route);
+                updateSizeToFreq(count);
+                System.out.println("Thread: " + Thread.currentThread().getId() + ", Count: " + count);
 
-                });
-                threads[i].start();
-            }
-
-            for(Thread thread : threads){
-                try {
-                    thread.join();
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
-            }
-printSizeToFreq();
+            });
+            threads[i].start();
         }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        printSizeToFreq();
+    }
 
     public static String generateRoute(String letters, int length) {
         Random random = new Random();
@@ -41,31 +41,33 @@ printSizeToFreq();
         return route.toString();
     }
 
-    public static int countTurns (String route) {
-            int count = 0;
-            for(char c : route.toCharArray()){
-                if(c == 'R') {
-                    count++;
-                }
+    public static int countTurns(String route) {
+        int count = 0;
+        for (char c : route.toCharArray()) {
+            if (c == 'R') {
+                count++;
             }
-            return count;
+        }
+        return count;
     }
 
-    public static synchronized void updateSizeToFreq(int count){
-            sizeToFreq.put(count,sizeToFreq.getOrDefault(count,0) +1);
+    public static void updateSizeToFreq(int count) {
+        synchronized (sizeToFreq) {
+            sizeToFreq.put(count, sizeToFreq.getOrDefault(count, 0) + 1);
+        }
     }
 
-    public static void printSizeToFreq(){
-            int maxCount = 0;
-            int maxFrequency = 0;
+    public static void printSizeToFreq() {
+        int maxCount = 0;
+        int maxFrequency = 0;
 
         System.out.println("Frequencies: ");
 
-        for(Map.Entry<Integer,Integer> entry : sizeToFreq.entrySet()){
+        for (Map.Entry<Integer, Integer> entry : sizeToFreq.entrySet()) {
             int count = entry.getKey();
             int frequency = entry.getValue();
 
-            if (frequency > maxFrequency){
+            if (frequency > maxFrequency) {
                 maxFrequency = frequency;
                 maxCount = count;
             }
